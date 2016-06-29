31e4d3a8ef9eda93d31c41fb8fd3b68b75c3308e
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-06-29 19:13:01 +0200

    Header template

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,7 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/templates/posts/posts_list.js';
-import '../../ui/layouts/main_layout.html';
+import '../../ui/layouts/main_layout.js';
 
 FlowRouter.route('/', {
   name: 'posts',
