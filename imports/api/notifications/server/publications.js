b5f96b8280ebadea61b2b38e15ffb47a1f62de4e
masoomi1 <saidmujtabamasoomi@gmail.com>
2017-02-24 15:53:16 +0000

    Update publications.js

--- imports/api/notifications/server/publications.js
+++ imports/api/notifications/server/publications.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Notifications } from '../notifications.js';
-
+the.Name (right<if(theName (StepAreTheSame))>)
 Meteor.publish('notifications', function () {
   return Notifications.find({ postAuthorId: this.userId, read: false });
 });
